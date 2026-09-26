0996d09007f7b9d3a04c285a4eeaaabf28f29bd1
Masahiro Nagano <kazeburo@gmail.com>
2026-09-26 23:34:30 +0900

    update mod

--- main_test.go
+++ main_test.go
@@ -39,7 +39,7 @@ func generateJSONLFile(b testing.TB, dir, filename string, numLines int) error {
 	defer file.Close()
 	r := rand.New(rand.NewPCG(1, 2))
 	for i := 0; i < numLines; i++ {
-		line := fmt.Sprintf(`{"time": "%s", "status": "%d", "reqtime": "%f", "host": "%s", "req": "%s", "method": "%s", "size": "%d", "ua": "%s"}`,
+		line := fmt.Sprintf(`{"time": "%s", "status": "%d", "reqtime": "%.3f", "host": "%s", "req": "%s", "method": "%s", "size": "%d", "ua": "%s"}`,
 			time.Now().Format(time.RFC3339),
 			200+i%5,
 			float64(r.IntN(500))/1000,
